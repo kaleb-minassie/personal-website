2f1bf95bf314656376b6332313bb89b838596582
Kaleb Minassie <126221144+kaleb-minassie@users.noreply.github.com>
2025-07-27 13:07:22 -0400

    Add files via upload

---
+++ pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <section id="home">
+      <h1>Welcome to My Portfolio</h1>
+      <p>I'm Kaleb, a Computer Engineering Student.</p>
+    </section>
+  );
+}
+
+export default Home;
